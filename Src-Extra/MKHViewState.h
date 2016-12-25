8f7d55f5e8d3839263c7e867c3fc0b5dfe8aa021
Maxim Khatskevich <maxim@khatskevi.ch>
2016-12-25 21:32:44 +0500

    Generated initial versions of key files with Xcode
    + set proper folder structure

---
+++ Src-Extra/MKHViewState.h
@@ -0,0 +1,19 @@
+//
+//  MKHViewState.h
+//  MKHViewState
+//
+//  Created by Maxim Khatskevich on 12/25/16.
+//  Copyright © 2016 Maxim Khatskevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKHViewState.
+FOUNDATION_EXPORT double MKHViewStateVersionNumber;
+
+//! Project version string for MKHViewState.
+FOUNDATION_EXPORT const unsigned char MKHViewStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKHViewState/PublicHeader.h>
+
+
---
+++ Tst/Main.swift
@@ -0,0 +1,23 @@
+//
+//  Main.swift
+//  MKHViewStateTst
+//
+//  Created by Maxim Khatskevich on 12/25/16.
+//  Copyright © 2016 Maxim Khatskevich. All rights reserved.
+//
+
+import XCTest
+
+//@testable
+import MKHViewState
+
+//===
+
+class Main: XCTestCase
+{
+    func testExample()
+    {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
